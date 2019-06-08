fa56e94528c974e7648bafe0848109e060e7494e
Mallaiya <mallaiyazap@gmail.com>
2019-06-08 14:26:18 +0530

    Updates

---
+++ precache-manifest.148e464918ace2ea01ae64210198c824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c24dbb18f0ce8be6075c2ffc53c6129",
+    "url": "/invoice-app/index.html"
+  },
+  {
+    "revision": "08bdf036c97e258dfc75",
+    "url": "/invoice-app/static/css/main.5e9f6fc0.chunk.css"
+  },
+  {
+    "revision": "c4d2d927e18841549307",
+    "url": "/invoice-app/static/js/2.5fd5d7c0.chunk.js"
+  },
+  {
+    "revision": "08bdf036c97e258dfc75",
+    "url": "/invoice-app/static/js/main.b9b1f5f5.chunk.js"
+  },
+  {
+    "revision": "f09f2a160332e41e6dfd",
+    "url": "/invoice-app/static/js/runtime~main.2e0e5192.js"
+  },
+  {
+    "revision": "127001bba29a84d040ab52e354c9728e",
+    "url": "/invoice-app/static/media/paper.127001bb.png"
+  }
+]);
